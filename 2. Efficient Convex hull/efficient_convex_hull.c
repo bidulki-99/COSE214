c88d5342d34e1d8b9f0056db6165982bae8b63fc
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-05-08 00:35:54 +0900

    Update efficient_convex_hull.c

--- 2. Efficient Convex hull/efficient_convex_hull.c
+++ 2. Efficient Convex hull/efficient_convex_hull.c
@@ -99,13 +99,13 @@ float distance( float a, float b, float c, t_point p) {
 void separate_points( t_point *points, int num_point, t_point from, t_point to, t_point *s1, t_point *s2, int *n1, int *n2) {
 	*n1 = 0;
 	*n2 = 0;
-	int a = to.y - from.y;
-	int b = from.x - to.x;
-	int c = from.x * to.y - from.y * to.x;
+	float a = to.y - from.y;
+	float b = from.x - to.x;
+	float c = from.x * to.y - from.y * to.x;
 
 	for (int i = 0; i < num_point; i++) {
 		t_point p = points[i];
-		int line = a * p.x + b * p.y - c;
+		float line = a * p.x + b * p.y - c;
 
 		if (line < 0)
 			s1[(*n1)++] = p;
@@ -257,6 +257,3 @@ int main( int argc, char **argv)
 	
 	return 0;
 }
-
-
-
